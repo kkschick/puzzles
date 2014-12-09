bc9b76b6361c5e479a3d982149f8c02df268b623
Katie Schick <kkschick@gmail.com>
2014-12-09 14:03:49 -0800

    Rewrote solution using list comprehension

--- euler01.py
+++ euler01.py
@@ -7,10 +7,8 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
 def sum_multiples_of_3_or_5():
-	accumulator = 0
-	for i in range(1000):
-		if i % 3 == 0 or i % 5 == 0:
-			accumulator += i
-	return accumulator
+
+	lst = [i for i in range(1000) if (i % 3 == 0 or i % 5 == 0)]
+	return sum(lst)
 
 print sum_multiples_of_3_or_5()
